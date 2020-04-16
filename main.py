94e0ddc82df08e8dc57cdb3142212eaddffb2fb5
Walter Irvin <walterdirvin@gmail.com>
2020-04-16 09:07:12 -0400

    changed serial port, tested car

--- main.py
+++ main.py
@@ -4,15 +4,18 @@ import time
 camera_device = 1  # 1 on devices with built-in camera, most likely 0 otherwise
 # noinspection PyArgumentList
 cap = cv2.VideoCapture(camera_device)
-device = serial.Serial(3)
-
-device.write("forward")
-time.sleep(5)
-device.write("straighten")
-time.sleep(5)
-device.write("right")
-time.sleep(5)
-device.write("left")
+device = serial.Serial("/dev/ttyACM0")
+forward = "forward"
+straight = "straighten"
+right = "right"
+left = "left"
+#device.write(forward.encode())
+#time.sleep(5)
+device.write(straight.encode())
+#time.sleep(5)
+#device.write(right.encode())
+#time.sleep(5)
+#device.write(left.encode())
 #while 1:
     #ret, frame = cap.read()
     #grayFrame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
